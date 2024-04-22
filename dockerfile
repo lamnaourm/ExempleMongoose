c80cfbe72191a3db06ce4e296317f59f26ec6a0b
lamnaourm <mohammed.lamnaour@gmail.com>
2024-04-22 15:52:52 +0100

    Exemple Mongoose

--- dockerfile
+++ dockerfile
@@ -2,12 +2,12 @@ FROM node
 
 WORKDIR /app
 
-COPY package.json .
+COPY package*.json .
 
-RUN npm install 
+RUN npm install
 
 COPY . .
 
 EXPOSE 3000
 
-CMD [ "npm", "start" ]
+CMD npm start
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
 import personne from './routes/Personne.js'
 
 const app = express()
 app.use(express.json())
-dotenv.config()
 
 const port = process.env.port || 3000
 
